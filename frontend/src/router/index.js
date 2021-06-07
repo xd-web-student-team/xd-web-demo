0146f1810057efc01ae20b9e76a098d1be46afcd
moshen21 <58246153+moshen21@users.noreply.github.com>
2021-06-07 14:39:42 +0800

    Update index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,6 +4,10 @@ import Login from '../views/chat/Login'
 import AdminLogin from '../views/admin/AdminLogin'
 import Register from '../views/chat/Register'
 import ChatRoom from '../views/chat/ChatRoom'
+import GroupChatRecord from '../views/admin/GroupChatRecord.vue'
+import PrivateChatRecord from '../views/admin/PrivateChatRecord.vue'
+import UserInfo from '../views/admin/UserInfo'
+import Home from '../views/admin/Home';
 
 Vue.use(VueRouter)
 
@@ -31,7 +35,36 @@ const routes = [
     name: 'ChatRoom',
     component: ChatRoom,
     hidden: true
-  }
+  },
+	{
+		path: '/home',
+		name: '用户管理',
+		component: Home,
+		iconCls: "fa fa-user",
+		children: [{
+			path: '/userinfo',
+			name: '用户信息管理',
+			component: UserInfo,
+		}]
+	},
+	{
+		path: '/home',
+		name: '聊天记录管理',
+		iconCls: 'fa fa-book',
+		component: Home,
+		children: [
+			{
+				path: '/groupChatRecord',
+				name: '群聊记录管理',
+				component: GroupChatRecord
+			},
+			{
+				path: '/privateChatRecord',
+				name: '私聊记录管理',
+				component: PrivateChatRecord
+			}
+		]
+	}
 ]
 //解决重复访问路由地址报错
 const originalPush = VueRouter.prototype.push;
